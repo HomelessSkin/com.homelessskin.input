ffce71153aeaa1b955e95a26613457b7d0e90914
HomelessSkin <105777886+HomelessSkin@users.noreply.github.com>
2026-07-31 18:50:46 +0500

    44

--- Scripts/Runtime/ECS/Components/OuterInput.cs
+++ Scripts/Runtime/ECS/Components/OuterInput.cs
@@ -8,45 +8,18 @@ namespace Input
 {
     public class OuterInput : IComponentData, ILogTarget
     {
-        [LogInfo] public string Platform = "overlay";
-        [LogInfo] public string RewardID;
         [LogInfo] public string Title;
         [LogInfo] public string ID;
+        [LogInfo] public string Source = "this";
+        [LogInfo] public string Agent = "unknown";
+        [LogInfo] public int Cost = 0;
+        [LogInfo] public string Message;
 
-        [LogInfo] public string UserID;
-        [LogInfo] public string Nick = "unknown";
-        [LogInfo] public int Points = 0;
+        public string UserID;
+        public string RewardID;
 
-        [LogInfo] public List<Part> UserInput;
-
-        public int Index;
-        public bool IsSlashMe;
-        public string NickColor;
-        public List<Icon> Badges;
-
-        public class Part : ILogTarget
-        {
-            [LogInfo] public Text Message;
-
-            public Icon Emote;
-            public Mention Reply;
-
-            public class Text : ILogTarget
-            {
-                [LogInfo] public string Content;
-            }
-
-            public class Mention
-            {
-                public string Nick;
-            }
-        }
-
-        public class Icon
-        {
-            public int Hash;
-            public int Index;
-        }
+        public List<int> Icons;
+        public List<int> Badges;
 
         public OuterInput() { }
         public OuterInput(string title)
